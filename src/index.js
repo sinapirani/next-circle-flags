d7b2fc217cf74f738218759657255fa3d315265b
sinapirani <sinapiranidev@gmail.com>
2023-08-02 09:19:24 +0330

    ignore height and ..

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import countries from './countries'
 import Image from 'next/image'
 
-const CDN_URL = "http://dl.koochaa.com"
+const CDN_URL = "http://dl.koochaa.com/"
 const FILE_SUFFIX = 'svg'
 
 const UNKNOWN_FLAG = 'xx'
@@ -11,19 +11,17 @@ const DEFAULT_WIDTH = 100
 const DEFAULT_QUALITY = 100
 
 
+
 /**
  * @param {string} countryCode
  * @param {string} cdnUrl
  * @param {Omit<import('../index').CircleFlagProps, "countryCode" | "cdnUrl">} otherProps
  */
-const getSvgProps = (countryCode, cdnUrl, fill, height, width, quality, otherProps) => ({
+const getSvgProps = (countryCode, cdnUrl, otherProps) => ({
   ...otherProps,
-  title: otherProps.title || countryCode,
-  height: otherProps.height || DEFAULT_HEIGHT,
-  width: otherProps.width || DEFAULT_WIDTH,
-  quality: otherProps.quality || DEFAULT_QUALITY,
+  title: otherProps?.title || countryCode,
+  quality: otherProps?.quality || DEFAULT_QUALITY,
   src: `${cdnUrl || CDN_URL}${countryCode}.${FILE_SUFFIX}`,
-  ...!otherProps.height && !otherProps.width && { fill: otherProps.fill },
 })
 
 /**
@@ -34,7 +32,8 @@ const parseCountryCode = (countryCode) =>
 /**
  * @param {import('../index').CircleFlagProps} param0
  */
-export const CircleFlag = ({ countryCode, cdnUrl, quality, height, width, ...otherProps }) => {
+
+export const CircleFlag = ({ countryCode, cdnUrl, ...otherProps }) => (
   <Image
     data-testid='circle-country-flag'
     {...getSvgProps(
@@ -43,6 +42,6 @@ export const CircleFlag = ({ countryCode, cdnUrl, quality, height, width, ...oth
       otherProps
     )}
 />
-}
+)
 
 export { countries }
